09e8126b0cba8b3f806ba990ce0283f2d851cf2e
Michael Waaler <michael@flyingtens.com>
2024-01-31 14:05:35 +0100

    refactor: moves component out of assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TextAreaWithFindHighlighting from "./assets/TextAreaWithFindHighlighting/TextAreaWithFindHighlighting.tsx";
+import TextAreaWithFindHighlighting from "./TextAreaWithFindHighlighting/TextAreaWithFindHighlighting.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
