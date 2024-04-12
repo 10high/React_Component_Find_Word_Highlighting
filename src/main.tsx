ecb678ecc8cecb8e7dd955c9393e13301ed23fb7
Michael Waaler <michael@flyingtens.com>
2024-04-12 15:27:51 +0200

    chore: updates main component for dev. Safe to ignore.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <TextAreaWithFindHighlighting
-      wordToHighlight="test"
+      wordsToHighlight={["test"]}
       isCaseSensitive={false}
       textAreaFormDataName="findWordTextArea"
       useRegularExpression={false}
