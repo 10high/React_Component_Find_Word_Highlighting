0775f05e53e264dcc03ee10f42602c6bba0c026d
Michael Waaler <michael@flyingtens.com>
2025-04-19 15:47:08 +0200

    perf: memoizes TextAreaWithFindHighlighting component

--- src/TextAreaWithFindHighlighting/TextAreaWithFindHighlighting.tsx
+++ src/TextAreaWithFindHighlighting/TextAreaWithFindHighlighting.tsx
@@ -1,5 +1,5 @@
 import styles from "./TextAreaWithFindHighlighting.module.css";
-import { useState } from "react";
+import { memo, useState } from "react";
 import Input from "./Input/Input";
 import Output from "./Output/Output";
 import { Props } from "./TextAreaWithFindHighlighting.interface";
@@ -24,38 +24,42 @@ const configureStyles = {
   },
 };
 
-export default function TextAreaWithFindHighlighting({
-  wordsToHighlight = ["test"],
-  isCaseSensitive = false,
-  textAreaFormDataName = "findWordTextArea",
-  useRegularExpression = true,
-}: Props) {
-  const [inputValue, setInputValue] = useState("");
-  const [selectionPositions, setSelectionPositions] = useState([0, 0]);
-  const [scrollTop, setScrollTop] = useState(0);
-  return (
-    <div
-      style={configureStyles.textArea}
-      className={styles.container}
-      tabIndex={0}
-    >
-      <Input
-        inputValue={inputValue}
-        setInputValue={setInputValue}
-        setSelectionPositions={setSelectionPositions}
-        textAreaFormDataName={textAreaFormDataName}
-        setScrollTop={setScrollTop}
-      />
-      <Output
-        inputValue={inputValue}
-        selectionPositions={selectionPositions}
-        wordsToHighlight={wordsToHighlight}
-        isCaseSensitive={isCaseSensitive}
-        textSelectionStyling={configureStyles.textSelection}
-        wordFindHighlightingStyling={configureStyles.wordFindHighlighting}
-        useRegularExpression={useRegularExpression}
-        scrollTop={scrollTop}
-      />
-    </div>
-  );
-}
+const TextAreaWithFindHighlighting = memo(
+  function TextAreaWithFindHighlighting({
+    wordsToHighlight = ["test"],
+    isCaseSensitive = false,
+    textAreaFormDataName = "findWordTextArea",
+    useRegularExpression = true,
+  }: Props) {
+    const [inputValue, setInputValue] = useState("");
+    const [selectionPositions, setSelectionPositions] = useState([0, 0]);
+    const [scrollTop, setScrollTop] = useState(0);
+    return (
+      <div
+        style={configureStyles.textArea}
+        className={styles.container}
+        tabIndex={0}
+      >
+        <Input
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          setSelectionPositions={setSelectionPositions}
+          textAreaFormDataName={textAreaFormDataName}
+          setScrollTop={setScrollTop}
+        />
+        <Output
+          inputValue={inputValue}
+          selectionPositions={selectionPositions}
+          wordsToHighlight={wordsToHighlight}
+          isCaseSensitive={isCaseSensitive}
+          textSelectionStyling={configureStyles.textSelection}
+          wordFindHighlightingStyling={configureStyles.wordFindHighlighting}
+          useRegularExpression={useRegularExpression}
+          scrollTop={scrollTop}
+        />
+      </div>
+    );
+  }
+);
+
+export default TextAreaWithFindHighlighting;
